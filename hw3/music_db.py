1fded3b1d358eb77ec114a58edc4fe8d5d9a3cc3
Clairie Zhao <clairie@diassmoot.rad.rutgers.edu>
2026-05-03 09:15:07 -0400

    hw 3 first commit

---
+++ hw3/music_db.py
@@ -0,0 +1,187 @@
+from typing import Tuple, List, Set
+
+def clear_database(mydb):
+    """
+    Deletes all the rows from all the tables of the database.
+    If a table has a foreign key to a parent table, it is deleted before 
+    deleting the parent table, otherwise the database system will throw an error. 
+
+    Args:
+        mydb: database connection
+    """
+    pass
+    
+def load_single_songs(mydb, single_songs: List[Tuple[str,Tuple[str...],str,str]]) -> Set[Tuple[str,str]]:
+    """
+    Add single songs to the database. 
+
+    Args:
+        mydb: database connection
+        
+        single_songs: List of single songs to add. Each single song is a tuple of the form:
+              (song title, genre names, artist name, release date)
+        Genre names is a tuple since a song could belong to multiple genres
+        Release date is of the form yyyy-dd-mm
+        Example 1 single song: ('S1',('Pop',),'A1','2008-10-01') => here song is of genre Pop
+        Example 2 single song: ('S2',('Rock', 'Pop),'A2','2000-02-15') => here song is of genre Rock and Pop
+
+    Returns:
+        Set[Tuple[str,str]]: set of (song,artist) for combinations that already exist 
+        in the database and were not added (rejected). 
+        Set is empty if there are no rejects.
+    """
+    pass
+
+def get_most_prolific_individual_artists(mydb, n: int, year_range: Tuple[int,int]) -> List[Tuple[str,int]]:   
+    """
+    Get the top n most prolific individual artists by number of singles released in a year range. 
+    Break ties by alphabetical order of artist name.
+
+    Args:
+        mydb: database connection
+        n: how many to get
+        year_range: tuple, e.g. (2015,2020)
+
+    Returns:
+        List[Tuple[str,int]]: list of (artist name, number of songs) tuples.
+        If there are fewer than n artists, all of them are returned.
+        If there are no artists, an empty list is returned.
+    """
+    pass
+
+def get_artists_last_single_in_year(mydb, year: int) -> Set[str]:
+    """
+    Get all artists who released their last single in the given year.
+    
+    Args:
+        mydb: database connection
+        year: year of last release
+        
+    Returns:
+        Set[str]: set of artist names
+        If there is no artist with a single released in the given year, an empty set is returned.
+    """
+    pass
+    
+def load_albums(mydb, albums: List[Tuple[str,str,str,str,List[str]]]) -> Set[Tuple[str,str]]:
+    """
+    Add albums to the database. 
+    
+    Args:
+        mydb: database connection
+        
+        albums: List of albums to add. Each album is a tuple of the form:
+              (album title, genre, artist name, release date, list of song titles) 
+        Release date is of the form yyyy-dd-mm
+        Example album: ('Album1','Jazz','A1','2008-10-01',['s1','s2','s3','s4','s5','s6'])
+
+    Returns:
+        Set[Tuple[str,str]: set of (album, artist) combinations that were not added (rejected) 
+        because the artist already has an album of the same title.
+        Set is empty if there are no rejects.
+    """
+    pass
+
+def get_top_song_genres(mydb, n: int) -> List[Tuple[str,int]]:
+    """
+    Get n genres that are most represented in terms of number of songs in that genre.
+    Songs include singles as well as songs in albums. 
+    
+    Args:
+        mydb: database connection
+        n: number of genres
+
+    Returns:
+        List[Tuple[str,int]]: list of tuples (genre,number_of_songs), from most represented to
+        least represented genre. If number of genres is less than n, returns all.
+        Ties broken by alphabetical order of genre names.
+    """
+
+def get_album_and_single_artists(mydb) -> Set[str]:
+    """
+    Get artists who have released albums as well as singles.
+
+    Args:
+        mydb; database connection
+
+    Returns:
+        Set[str]: set of artist names
+    """
+    
+def load_users(mydb, users: List[str]) -> Set[str]:
+    """
+    Add users to the database. 
+
+    Args:
+        mydb: database connection
+        users: list of usernames
+
+    Returns:
+        Set[str]: set of all usernames that were not added (rejected) because 
+        they are duplicates of existing users.
+        Set is empty if there are no rejects.
+    """
+    pass
+
+def load_song_ratings(mydb, song_ratings: List[Tuple[str,Tuple[str,str],int, str]]) -> Set[Tuple[str,str,str]]:
+    """
+    Load ratings for songs, which are either singles or songs in albums. 
+
+    Args:
+        mydb: database connection
+        song_ratings: list of rating tuples of the form:
+            (rater, (artist, song), rating, date)
+        
+        The rater is a username, the (artist,song) tuple refers to the uniquely identifiable song to be rated.
+        e.g. ('u1',('a1','song1'),4,'2021-11-18') => u1 is giving a rating of 4 to the (a1,song1) song.
+
+    Returns:
+        Set[Tuple[str,str,str]]: set of (username,artist,song) tuples that are rejected, for any of the following
+        reasongs:
+        (a) username (rater) is not in the database, or
+        (b) username is in database but (artist,song) combination is not in the database, or
+        (c) username has already rated (artist,song) combination, or
+        (d) everything else is legit, but rating is not in range 1..5
+        
+        An empty set is returned if there are no rejects.  
+    """
+    pass
+
+def get_most_rated_songs(mydb, year_range: Tuple[int,int], n: int) -> List[Tuple[str,str,int]]:
+    """
+    Get the top n most rated songs in the given year range (both inclusive), 
+    ranked from most rated to least rated. 
+    "Most rated" refers to number of ratings, not actual rating scores. 
+    Ties are broken in alphabetical order of song title. If the number of rated songs is less
+    than n, all rates songs are returned.
+    
+    Args:
+        mydb: database connection
+        year_range: range of years, e.g. (2018-2021), during which ratings were given
+        n: number of most rated songs
+
+    Returns:
+        List[Tuple[str,str,int]: list of (song title, artist name, number of ratings for song)   
+    """
+    pass
+
+def get_most_engaged_users(mydb, year_range: Tuple[int,int], n: int) -> List[Tuple[str,int]]:
+    """
+    Get the top n most engaged users, in terms of number of songs they have rated.
+    Break ties by alphabetical order of usernames.
+
+    Args:
+        mydb: database connection
+        year_range: range of years, e.g. (2018-2021), during which ratings were given
+        n: number of users
+
+    Returns:
+        List[Tuple[str, int]]: list of (username,number_of_songs_rated) tuples
+    """
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
